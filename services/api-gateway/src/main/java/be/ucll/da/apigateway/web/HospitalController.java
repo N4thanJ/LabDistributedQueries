4b7e5b09e46b864268cf54e1d9e21664c533dda0
N4thanJ <nathan.jordens@student.ucll.be>
2025-12-26 17:46:29 +0100

    temp

--- services/api-gateway/src/main/java/be/ucll/da/apigateway/web/HospitalController.java
+++ services/api-gateway/src/main/java/be/ucll/da/apigateway/web/HospitalController.java
@@ -2,7 +2,18 @@ package be.ucll.da.apigateway.web;
 
 import be.ucll.da.apigateway.api.HospitalApiDelegate;
 
+import be.ucll.da.apigateway.api.HospitalApiDelegate;
 import be.ucll.da.apigateway.api.model.*;
+import be.ucll.da.apigateway.client.appointment.api.AppointmentApi;
+import be.ucll.da.apigateway.client.doctor.api.DoctorApi;
+import be.ucll.da.apigateway.client.doctor.model.ApiDoctor;
+import be.ucll.da.apigateway.client.patient.api.PatientApi;
+import be.ucll.da.apigateway.client.patient.model.ApiPatient;
+import be.ucll.da.apigateway.cqrs.Appointment;
+import be.ucll.da.apigateway.cqrs.AppointmentRepository;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +23,30 @@ import java.time.format.DateTimeFormatter;
 
 @Component
 public class HospitalController implements HospitalApiDelegate {
+    private final AppointmentApi appointmentApi;
+    private final PatientApi patientApi;
+    private final DoctorApi doctorApi;
+    private final EurekaClient discoveryClient;
+    private final CircuitBreakerFactory<?, ?> circuitBreakerFactory;
+
+    public HospitalController(AppointmentApi appointmentApi, PatientApi patientApi, DoctorApi doctorApi, EurekaClient discoveryClient, CircuitBreakerFactory<?, ?> circuitBreakerFactory) {
+        this.appointmentApi = appointmentApi;
+        this.patientApi = patientApi;
+        this.doctorApi = doctorApi;
+        this.discoveryClient = discoveryClient;
+        this.circuitBreakerFactory = circuitBreakerFactory;
+    }
 
     @Override
     public ResponseEntity<Void> apiV1AppointmentConfirmationPost(ApiAppointmentConfirmation apiAppointmentConfirmation) {
-        throw new RuntimeException("Implement me!!");
+        appointmentApi.getApiClient().setBasePath(discoveryClient.getNextServerFromEureka("appointment-service", false).getHomePageUrl());
+
+        var confirmation = new be.ucll.da.apigateway.client.appointment.model.ApiAppointmentConfirmation();
+        confirmation.appointmentRequestNumber(apiAppointmentConfirmation.getAppointmentRequestNumber());
+        confirmation.setAcceptProposedAppointment(apiAppointmentConfirmation.getAcceptProposedAppointment());
+
+        return circuitBreakerFactory.create("appointmentApi")
+                .run(() -> appointmentApi.apiV1AppointmentConfirmationPostWithHttpInfo(confirmation));
     }
 
     @Override
